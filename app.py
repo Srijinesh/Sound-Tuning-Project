6b00dd1c3d4d6376a6916a112305713ce13ff8a9
Srijinesh Alanka <srijinesh.alanka@gmail.com>
2024-04-18 17:29:15 -0400

    Intital commit of starter code. Two rough html files

--- app.py
+++ app.py
@@ -15,12 +15,22 @@ def home():
 
 @app.route('/index', methods=['POST'])
 def index():
-        audio = request.files['file']
-        file_path = '/tmp/' + audio.filename
+        path = os.getcwd()
+        if os.path.isdir(path+'/audio_files')==False:
+             os.mkdir("audio_files")
+        file = request.files['file']
+        path = path+'/audio_files'
+        file_path = os.path.join(path, file.filename)
+        file.save(file_path)
         y,sr = librosa.load(file_path)
-        chroma = librosa.feature.chroma_stft(y, sr=sr)
-        key = np.argmax(chroma)
-        return render_template('index.html',key=key)
+        chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+        #key = librosa.beat.tempo(y=y, sr=sr)
+
+        mean_chroma = np.mean(chroma, axis=1)
+        chroma_to_key = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        key = np.argmax(mean_chroma)
+        key_scale = chroma_to_key[key]
+        return render_template('index.html',key=key,key_scale = key_scale)
 
 
 if __name__=='__main__':
