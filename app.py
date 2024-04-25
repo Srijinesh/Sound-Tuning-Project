63a82a618d98f25fd98664c9a507cf981736fc64
Srijinesh Alanka <srijinesh.alanka@gmail.com>
2024-04-25 14:35:52 -0400

    merging html pages, still under progress

--- app.py
+++ app.py
@@ -11,28 +11,34 @@ from bs4 import BeautifulSoup as bs
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=["GET", "POST"])
 def home():
-        path = os.getcwd()
-        if os.path.isdir(path+'/audio_files')==False:
-             os.mkdir("audio_files")
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "No file part"
+        
         file = request.files['file']
-        path = path+'/audio_files'
-        file_path = os.path.join(path, file.filename)
+        if file.filename == '':
+            return "No selected file"
+        
+        # Continue with file processing
+        path = os.getcwd()
+        if not os.path.isdir(path+'/audio_files'):
+             os.mkdir("audio_files")
+        
+        file_path = os.path.join(path+'/audio_files', file.filename)
         file.save(file_path)
-        y,sr = librosa.load(file_path)
+        
+        y, sr = librosa.load(file_path)
         chroma = librosa.feature.chroma_stft(y=y, sr=sr)
-        #key = librosa.beat.tempo(y=y, sr=sr)
-
         mean_chroma = np.mean(chroma, axis=1)
         chroma_to_key = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
         key = np.argmax(mean_chroma)
         key_scale = chroma_to_key[key]
-        return render_template('home.html',key=key,key_scale = key_scale)
+        
+        return render_template('home.html', key=key, key_scale=key_scale)
+    else:
+        return render_template('home.html')
 
-
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
